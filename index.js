9f2d065668854bfad9ef08438abec763b05ff9bf
Noppanit Charassinvichai <noppanit.c@gmail.com>
2016-05-03 21:17:04 -0400

    Accept message

--- index.js
+++ index.js
@@ -14,14 +14,31 @@ server.start((err) => {
   console.log('Server running at:', server.info.uri);
 });
 
-server.route({
+server.route([{
   method: 'GET',
   path: '/webhook',
-  handler: function (req, reply) {
-    console.log(verifyToken);
+  handler: (req, reply) => {
     if (req.query['hub.verify_token'] === verifyToken) {
       return reply(req.query['hub.challenge']);
     }
     return reply('Error, wrong validation token');
   }
-});
+},
+{
+  method: 'POST',
+  path: '/webhook',
+  handler: (req, reply) => {
+    messaging_events = req.payload.entry[0].messaging;
+    for (i = 0; i < messaging_events.length; i++) {
+      let event = req.payload.entry[0].messaging[i];
+      let sender = event.sender.id;
+      if (event.message && event.message.text) {
+        let text = event.message.text;
+        console.log(text);
+      }
+    }
+    reply('Accepted').code(200)
+  }
+}
+]);
+
