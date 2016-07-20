eaadd90e0ca982e84223ae7c682600528f75da5d
Noppanit Charassinvichai <noppanit.c@gmail.com>
2016-07-19 21:15:37 -0400

    Upgrade to node-wit 4.0.0

--- app/services/wit.js
+++ app/services/wit.js
@@ -1,47 +1,40 @@
 'use strict';
 
 const _ = require('lodash');
+const Promise = require('bluebird');
+
 const Facebook = require('./facebook');
 const sessions = require('./sessions');
 const keepr = require('./keepr');
+const config = require('../../config');
 
 module.exports = {
   actions: {
-    say: (sessionId, context, message, cb) => {
+    send: (request, response) => {
+      const sessionId = request.sessionId;
+      const context = request.context;
+      const entities = request.entities;
       const recipientId = sessions.getSessions()[sessionId].fbid;
       if (recipientId) {
-          Facebook.sendTextMessage(recipientId, message).then(() => {
-            cb();
-          }).catch(() => {
-            cb();
-          });
+          return Facebook.sendTextMessage(recipientId, message);
       } else {
-        console.log('Oops! Couldn\'t find user for session:', sessionId);
-        cb();
+        return Promise.reject('Oops! Couldn\'t find user for session: ' + sessionId);
       }
     },
-    ['fetch_latest_news_by_keyword'](sessionId, context, entities, cb) {
-      console.log('fetching latest news by keyword');
-      console.log(context);
-      console.log(entities);
+    ['fetch_latest_news_by_keyword'](request) {
+      const entities = request.entities;
+      const sessionId = request.sessionId;
+      const context = require.context;
       const recipientId = sessions.getSessions()[sessionId].fbid;
       if (recipientId) {
-
-        keepr.latestNewsByKeyword(entities.keyword).then((response) => {
+        return keepr.latestNewsByKeyword(entities.keyword).then((response) => {
           const numbers = response.numbers;
-
           const firstBreakingNews = numbers[1].urls[0].expanded_url;
           
-          console.log('Messages');
-          console.log(firstBreakingNews);
-          Facebook.sendTextMessage(recipientId, firstBreakingNews).then(() => {
-            cb(context);
-          }).catch(() => {
-            cb(context);
-          });
+          return Facebook.sendTextMessage(recipientId, firstBreakingNews);
         });
       }
-      return cb(context);
+      return Promise.reject('Oops! Couldn\'t find user for session: ' + sessionId);
     },
   }
 };
--- witInteractive.js
+++ witInteractive.js
@@ -5,5 +5,8 @@ const Wit = require('node-wit').Wit;
 const witService  = require('./app/services/wit');
 const config = require('./config');
 
-const client = new Wit(config.Wit.serverToken, witService.actions);
+const client = new Wit({
+  accessToken: config.Wit.serverToken, 
+  actions: witService.actions
+});
 client.interactive();
