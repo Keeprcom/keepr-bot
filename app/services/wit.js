f52864541c1f5a925ceecb6f442dff00ae08098d
Noppanit Charassinvichai <noppanit.c@gmail.com>
2016-07-31 19:18:45 -0400

    Print keyword

--- app/services/wit.js
+++ app/services/wit.js
@@ -26,6 +26,7 @@ module.exports = {
       const sessionId = request.sessionId;
       const context = require.context;
       const keyword = entities.local_search_query[0].value;
+      console.log('Keyword' + keyword);
 
       const recipientId = sessions.getSessions()[sessionId].fbid;
       if (recipientId) {
